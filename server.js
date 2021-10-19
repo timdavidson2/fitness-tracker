3bc0ed8f1bef4540372f706b8758a6b1ad39b2e7
Tim Davidson <timdavidson2@gmail.com>
2021-10-19 08:56:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/serene-basin",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/api.js"));
