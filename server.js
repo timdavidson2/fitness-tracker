42de2a6b20411088397c616d5213e933eca8fb22
Tim Davidson <timdavidson2@gmail.com>
2021-10-18 21:39:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/serene-basin",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
