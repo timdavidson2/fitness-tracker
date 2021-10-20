948e56caaabe608280fb914ac1487a9909efd18a
Tim Davidson <timdavidson2@gmail.com>
2021-10-20 18:31:43 -0400

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -1,13 +1,16 @@
-var path = require("path");
+const router = require("express").Router();
+const path = require("path");
 
-module.exports = function (app) {
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-  app.get("/stats", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
-  app.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
-};
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
