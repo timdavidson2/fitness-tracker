8bc2da09d965d8c50f947d1d398fd56c5e828915
Tim Davidson <timdavidson2@gmail.com>
2021-10-20 11:40:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker",
+  process.env.MONGODB_URI || "mongodb://localhost/fitness-traker",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
