3918c3724d59eb8da5962399e69cfd61dc01f349
Tim Davidson <timdavidson2@gmail.com>
2021-10-20 18:43:44 -0400

    Update workouts.js

--- models/workouts.js
+++ models/workouts.js
@@ -1,41 +1,40 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var workoutSchema = new Schema({
-  day: { type: Date, default: Date.now },
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
   exercises: [
     {
-      name: {
-        type: String,
-        // required: "Enter the name of the exercise"
-      },
       type: {
         type: String,
-        // required: "Enter the type of the exercise"
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
       },
       duration: {
         type: Number,
-        // required: "Enter the duration for the exercise"
       },
       weight: {
         type: Number,
-        // required: "Enter a weight"
       },
       reps: {
         type: Number,
-        // required: "Enter the amount of reps for the exercise"
       },
       sets: {
         type: Number,
-        // required: "Enter the amount of sets for the exercise"
       },
       distance: {
         type: Number,
       },
     },
   ],
+  totalDuration: {
+    type: Number,
+  },
 });
-
-var Workout = mongoose.model("workout", workoutSchema);
-
+const Workout = mongoose.model("workouts", workoutSchema);
 module.exports = Workout;
